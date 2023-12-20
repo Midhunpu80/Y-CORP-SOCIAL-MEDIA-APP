694fbb1666266bc4a1832b40c54d7e4eada664fe
Midhunpu80 <midhunpu80@gmail.com>
2023-12-20 21:18:49 +0530

    solved some bugs

--- lib/view/screen/notificationscreen/notificationscreen.dart
+++ lib/view/screen/notificationscreen/notificationscreen.dart
@@ -3,11 +3,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:sizer/sizer.dart';
+import 'package:social_syn/view/service/followingandunfollowing/following.dart';
 import 'package:social_syn/view/utility/alltext.dart';
 import 'package:social_syn/view/utility/colors.dart';
 
 class notificationscreen extends StatelessWidget {
-  const notificationscreen({super.key});
+  following_service newfollow = following_service();
+
+  notificationscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -42,72 +45,98 @@ class notificationscreen extends StatelessWidget {
                 : SingleChildScrollView(
                     child: Column(
                       children: [
-                        Container(
-                          height: 89.h,
-                          width: 100.w,
-                          color: bl,
-                          child: ListView.separated(
-                              itemBuilder: (context, index) {
-                                final snap = snapshot.data?.docs[index];
+                        snapshot.data!.docs.isEmpty
+                            ? Center(
+                                child: Container(
+                                child: alltext(
+                                    txt: "FA",
+                                    col: wh,
+                                    siz: 13.sp,
+                                    wei: FontWeight.bold,
+                                    max: 1),
+                              ))
+                            : Container(
+                                height: 89.h,
+                                width: 100.w,
+                                color: bl,
+                                child: ListView.separated(
+                                    itemBuilder: (context, index) {
+                                      final snap = snapshot.data?.docs[index];
 
-                                return ListTile(
-                                  trailing: snap?['case'] == true
-                                      ? Container(
-                                          height: 5.h,
-                                          width: 10.w,
-                                          color: yl,
-                                          child: Image(
-                                            image: NetworkImage(snap!['img']),
-                                            fit: BoxFit.cover,
-                                          ),
-                                        )
-                                      : Container(
-                                          height: 4.h,
-                                          width: 25.w,
-                                          decoration: BoxDecoration(
-                                              color:
-                                                  snap?['follower'] ? gy : yl,
-                                              borderRadius:
-                                                  BorderRadius.circular(1.h)),
-                                          child: Center(
-                                            child: snap?['follower']
-                                                ? alltext(
-                                                    txt: "following",
-                                                    col: wh,
-                                                    siz: 7.sp,
-                                                    wei: FontWeight.bold,
-                                                    max: 1)
-                                                : alltext(
-                                                    txt: "FOLLOW",
-                                                    col: bl,
-                                                    siz: 7.sp,
-                                                    wei: FontWeight.bold,
-                                                    max: 1),
-                                          ),
+                                      return ListTile(
+                                        trailing: snap?['case'] == true
+                                            ? Container(
+                                                height: 5.h,
+                                                width: 10.w,
+                                                color: yl,
+                                                child: Image(
+                                                  image: NetworkImage(
+                                                      snap!['img']),
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              )
+                                            : InkWell(
+                                                onTap: () async {
+                                                  await newfollow
+                                                      .followingtheuser(
+                                                    istru: snap?['follower'],
+                                                    usersid: snap!.id,
+                                                    uid: FirebaseAuth.instance
+                                                        .currentUser!.uid,
+                                                  );
+                                                },
+                                                child: Container(
+                                                  height: 4.h,
+                                                  width: 25.w,
+                                                  decoration: BoxDecoration(
+                                                      color: snap?['follower']
+                                                          ? gy
+                                                          : yl,
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              1.h)),
+                                                  child: Center(
+                                                    child: snap?['follower']
+                                                        ? alltext(
+                                                            txt: "following",
+                                                            col: wh,
+                                                            siz: 7.sp,
+                                                            wei:
+                                                                FontWeight.bold,
+                                                            max: 1)
+                                                        : alltext(
+                                                            txt: "FOLLOW",
+                                                            col: bl,
+                                                            siz: 7.sp,
+                                                            wei:
+                                                                FontWeight.bold,
+                                                            max: 1),
+                                                  ),
+                                                ),
+                                              ),
+                                        subtitle: alltext(
+                                            txt: snap!['messege'],
+                                            col: wh,
+                                            siz: 8.sp,
+                                            wei: FontWeight.bold,
+                                            max: 1),
+                                        leading: CircleAvatar(
+                                          backgroundImage:
+                                              NetworkImage(snap['profile']),
                                         ),
-                                  subtitle: alltext(
-                                      txt: snap!['messege'],
-                                      col: wh,
-                                      siz: 8.sp,
-                                      wei: FontWeight.bold,
-                                      max: 1),
-                                  leading: CircleAvatar(
-                                    backgroundImage:
-                                        NetworkImage(snap['profile']),
-                                  ),
-                                  title: alltext(
-                                      txt: snap?['name'],
-                                      col: wh,
-                                      siz: 12.sp,
-                                      wei: FontWeight.bold,
-                                      max: 1),
-                                );
-                              },
-                              separatorBuilder: (context, index) {
-                                return SizedBox();
-                              },
-                              itemCount: snapshot.data!.docs.length),
-                        )
+                                        title: alltext(
+                                            txt: snap?['name'],
+                                            col: wh,
+                                            siz: 12.sp,
+                                            wei: FontWeight.bold,
+                                            max: 1),
+                                      );
+                                    },
+                                    separatorBuilder: (context, index) {
+                                      return SizedBox();
+                                    },
+                                    itemCount: snapshot.data!.docs.length),
+                              )
                       ],
                     ),
                   );
